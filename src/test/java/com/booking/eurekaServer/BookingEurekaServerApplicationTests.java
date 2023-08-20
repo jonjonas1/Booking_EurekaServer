60727a087bafe98ca69b4cf3fe23ec48d7f2331a
Jon J <jj@>
2023-08-19 22:44:04 -0500

    install client discovery

---
+++ src/test/java/com/booking/eurekaServer/BookingEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
